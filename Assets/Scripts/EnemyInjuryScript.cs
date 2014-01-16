6c388613adc1f4a4c71fbdedcb45211692a7c4aa
DevenSmith <Deven.smith@students.aie.edu.au>
2014-01-16 11:39:45 -0800

    Added Attack Types
    set up quick different attack type shots

---
+++ Assets/Scripts/EnemyInjuryScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyInjuryScript : MonoBehaviour {
+
+	enum InjuryTypes {Straight, Verticle, Horizontal};
+	InjuryTypes injuryType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void RecieveInjury(AttackTypes typeOfAttack)
+	{
+		if(typeOfAttack == AttackTypes.Straight)
+		{
+			renderer.material.color = Color.red;
+		}
+		if(typeOfAttack == AttackTypes.Horizontal)
+		{
+			renderer.material.color = Color.blue;
+		}
+		if(typeOfAttack == AttackTypes.Verticle)
+		{
+			renderer.material.color = Color.green;
+		}
+
+	}
+}
