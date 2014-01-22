5a4ccba792a369760c2ca33c95222af6b842cc6d
Patrick Ryan <patrick.ryan@students.aie.edu.au>
2014-01-21 20:32:43 -0800

    replacing files from Friday

---
+++ Assets/Scripts/EnemyInjuryScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyInjuryScript : MonoBehaviour {
+
+	enum InjuryTypes {Straight, Verticle, Horizontal};
+	InjuryTypes injuryType;
+	public bool hasRagdoll;
+	public GameObject ragdoll;
+	GameObject hitByBullet;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void SetImpact(GameObject bullet)
+	{
+		hitByBullet = bullet;
+	}
+
+	void RecieveInjury(AttackTypes typeOfAttack)
+	{
+		if(!hasRagdoll)
+		{
+			if(typeOfAttack == AttackTypes.Straight)
+			{
+				renderer.material.color = Color.red;
+			}
+			if(typeOfAttack == AttackTypes.Horizontal)
+			{
+				renderer.material.color = Color.blue;
+			}
+			if(typeOfAttack == AttackTypes.Verticle)
+			{
+				renderer.material.color = Color.green;
+			}
+		}
+		else
+		{
+			GameObject child = (GameObject) Instantiate(ragdoll, transform.position, transform.rotation);
+
+
+			RigidBodyScript ragdollscript = child.GetComponent<RigidBodyScript>();
+			ragdollscript.SendMessage("SetImpact", hitByBullet);
+
+			gameObject.SetActive(false);
+		}
+
+	}
+}
---
+++ Assets/Scripts/ShootScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class ShootScript : MonoBehaviour {
+	public AttackTypes attackType;
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		attackType = AttackTypes.Straight;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			Debug.Log(attackType);
+			GameObject bullet = (GameObject) Instantiate(bulletPrefab, transform.position, transform.rotation);
+			BulletScript bs = bullet.GetComponent<BulletScript>();
+			bs.attackType = attackType;
+		}
+	}
+}
